4e4d5454d3f447f5a63ca3661ae5e0d1fd79d1cc
Antoine Vandenheste <antoine.vandenheste@ullink.com>
2015-09-30 14:36:49 +0800

    added device info

--- random.py
+++ random.py
@@ -9,9 +9,21 @@ def update():
 	for x in os.listdir("newfiles"):
 		shutil.copy2("newfiles/"+x, "files/"+x)
 
+def downloadUpdate():
+	print("pretending to download update")
+
+def getCurrentInfo():
+	with open("info.txt") as f:
+		content = f.readlines()[0]
+		info = {}
+		for i in content.split(","):
+			info[i.split("=")[0]] = [i.split("=")[1]] 
+	return info
+
 print("Checking for updates")
 print("Simulate downloaded updates")
 print("Update starting...")
 update()
 print("Updated complete")
 
+print(getCurrentInfo()["SerialID"])
